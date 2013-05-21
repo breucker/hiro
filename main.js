addb99634f6672bc813d2b6f6d6b2d7ad4972065
breucker <breucker@ifris.org>
2013-05-21 03:24:40 +0200

    added form update and new skill

--- main.js
+++ main.js
@@ -11,35 +11,33 @@ if (Meteor.isClient) {
     };
     //sheet events
     Template.sheet.events({
-    	'click #addSkill': function(){
-    		Skills.insert({domain:'Guns', sector:'Ray', name:'Laser', score: Math.floor(Random.fraction()*10)*5});
-    	},
-    	'click #removeSkill': function(){
-    		//fixme
-    		console.log("remove a skill");
-    	},
-    	'click #truncateSkills': function(){
-    		Skills.remove({});
-    	}
+    	
+    	
+    	// 'click #truncateSkills': function(){
+    	// 	Skills.remove({});
+    	// }
     });
 
     //skill events
     Template.skill.events({
-    	'dblclick': function () {
-    	 	console.log('skill'+this._id+' editing');
+    	// 'dblclick': function () {
+    	//  	console.log('skill'+this._id+' editing');
+   		//  },
+   		 'dblclick' : function () {
+   		 	$(domain).attr('value',this.domain);
+        $(sector).attr('value',this.sector);
+        $(skill).attr('value',this.name);
+        $(score).attr('value',parseInt(this.score));
+        $(idSkill).attr('value',this._id);
    		 },
-   		 'click' : function () {
-   		 	$(this).addClass('active');
-    	  	console.log(this._id+' - '+this.name+' active');
-   		 },
-   		 'mouseover' : function(){
-   		 	$(this).addClass('active');
-    	 	console.log(this._id+' - '+this.name+' hover');
-   		 },
-   		 'mouseout' : function(){
-   		 	$(this).removeClass('active');
-    	  	console.log(this._id+' - '+this.name+' out');
-   		 },
+   		 // 'mouseover' : function(){
+   		 // 	$(this).addClass('active');
+    	 // 	console.log(this._id+' - '+this.name+' hover');
+   		 // },
+   		 // 'mouseout' : function(){
+   		 // 	$(this).removeClass('active');
+    	 //  	console.log(this._id+' - '+this.name+' out');
+   		 // },
    		 'click .scoreUp' : function(){
    		 	Skills.update(this._id,{$inc: {score: 1}});
    		 },
@@ -52,6 +50,35 @@ if (Meteor.isClient) {
    		 },
 
   	});
+
+    Template.skillUpdate.domains = function(){
+      var domainList = ['"Art"', '"Drive"', '"Guns"'];
+      return domainList;
+    }
+    Template.skillUpdate.sectors = function(){
+      var domainList = ['"Art"', '"Drive"', '"Guns"'];
+      return domainList;
+    }
+    Template.skillUpdate.skills = function(){
+      var domainList = ['"Art"', '"Drive"', '"Guns"'];
+      return domainList;
+    }
+
+    Template.skillUpdate.events({
+      'submit': function(e){
+        e.preventDefault();
+      },
+      'click #addSkill' : function(){
+        Skills.insert({domain: domain.value, sector: sector.value, name: skill.value, score: parseInt(score.value)});
+        formSkill.reset();
+        console.log("submit");
+        console.log(formSkill);
+       },
+      'click #updSkill' : function(){
+        console.log("update"+idSkill.value);
+        Skills.update(idSkill.value,{$set: {domain: domain.value, sector: sector.value, name: skill.value, score: parseInt(score.value)}});
+       },
+    });
 }
 
 //insert test datas
